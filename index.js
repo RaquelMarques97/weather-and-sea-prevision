79704c2c358fa4e91697d03564d27bec270dac39
Raquel Marques <araquelfmarques@gmail.com>
2020-12-10 17:46:45 +0000

    change wind arrow direction

--- index.js
+++ index.js
@@ -6,13 +6,10 @@ function load(){
   $.get("https://api.openweathermap.org/data/2.5/onecall?lat=37.17823140510965&lon=-7.4497216680432174&units=metric&lang=pt&appid=12c1168f34c81c351be0582a533ebf58", function(data, status) {});
 }
 
-
   $(document).ready(function() {
     $.get("https://api.openweathermap.org/data/2.5/onecall?lat=37.17823140510965&lon=-7.4497216680432174&units=metric&lang=pt&appid=12c1168f34c81c351be0582a533ebf58", function(data, status) {
-  
-  
+  console.log(data);
       var img = data.current.weather[0].icon;
-      console.log(data);
       var sunset=new Date(data.current.sunset*1000);
       var sunseth=sunset.getHours();
       var sunsetm=sunset.getMinutes();
@@ -25,10 +22,11 @@ function load(){
       $('#current-icon').load(`animated/${img}.svg`);
       $("#vel").text( (data.current.wind_speed * 3.6).toFixed(1) + `km/h`);
       $("#dir").text( data.current.wind_deg + `º`);
-      $("#sensation").text(`sens:` + Math.round(data.current.feels_like) + `ºC`);
+      $("#sensation").text(`sens: ` + Math.round(data.current.feels_like) + `º`);
       $("#description").text(data.current.weather[0].description);
       $("#sunset").text(`${sunseth}:${sunsetm}`);
       $("#sunrise").text(`${sunriseh}:${sunrisem}`);
+      $('#wind-dir').css({'transform' : 'rotate('+ data.current.wind_deg +'deg)'});
   
   
       var hours = data.hourly;
@@ -45,7 +43,7 @@ function load(){
             .text(time.getHours() + ":00")
           )
           .append($('<div class="hr-icon">').load(`animated/${hour.weather[0].icon}.svg`))
-          .append($('<div class="hr-temp">').text(Math.round(hour.temp) + `ºC`))
+          .append($('<div class="hr-temp">').text(Math.round(hour.temp) + `º`))
           .append($('<div style="display:flex; position:absolute;margin-left:15px;" class="hr-pop">').text(Math.round(hour.pop) + `%`)).append('<img style="height:20px;margin-left:30px;" src="animated/raindrop.png"/>'); 
   
         $('#hours .list').append(element);     
@@ -67,7 +65,7 @@ function load(){
         weekday[6] = "Sábado";
         
         $('#day').append('<p style="font-weight:bold;height:64px;margin:0;">'+weekday[date.getDay()]+'</p>');
-        $('#day-pop').append('<p style="height:64px;margin:0;">'+Math.round(day.pop)+'%</p>').append('<img style="height:20px;margin-left:30px;position:absolute;margin-top:-63px;" src="animated/raindrop.png"/>');
+        $('#day-pop').append('<p style="height:64px;margin:0;">'+Math.round(day.pop)+'%</p>').append('<img style="height:20px;margin-left:30px;position:absolute;margin-top:-63px;z-index:20;" src="animated/raindrop.png"/>');
         $('#day-icon').append($('<div  class="day-icon">').load(`animated/${day.weather[0].icon}.svg`));       
         $('#day-temps').append('<p style="height:64px;margin:0;">'+Math.round(day.temp.max)+'º/'+Math.round(day.temp.min)+'º</p>');
       });
